5ee7b47e6b0e53ca0e03b2838b11c8fd10fccfac
mnoss <mnoss.site>
2024-07-12 12:40:29 -0400

    inital

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import JSConfetti from 'js-confetti'
+import webCards from './components/webCards.vue'
+
+const confetti = new JSConfetti()
+
+function showConfetti() {
+  confetti.addConfetti({
+    emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+    emojiSize: 50,
+    confettiNumber: 6
+  })
+}
+</script>
+
+<template>
+  <webCards @click="showConfetti"> hello world! </webCards>
+</template>
+
+<style></style>
